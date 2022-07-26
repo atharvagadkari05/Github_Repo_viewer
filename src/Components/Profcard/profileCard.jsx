e3101fa4ff81a399e47671a43f9df7b9d384a361
atharvagadkari05 <atharvagadkari0512@gmail.com>
2022-07-27 01:33:33 +0530

    features added

--- src/Components/Profcard/profileCard.jsx
+++ src/Components/Profcard/profileCard.jsx
@@ -3,13 +3,13 @@ import "./card.css";
 import Modalcomp from "../../Components/Modal/modal";
 
 function ProfileCard(props) {
-  const [modalisOpen, setModal] = useState(false);
+
 
   return (
     <div className="ProfCard">
-      <div class="m-4 w-max  p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div class="m-4 w-[450px]  p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             {props.data.name}
           </h5>
         </a>
@@ -19,15 +19,15 @@ function ProfileCard(props) {
         <button
           href="#git"
           onClick={() => {
-            setModal(true);
+            props.openmodal(true);
           }}
-          class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="inline-flex items-center py-1 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           View
         </button>
       </div>
 
-      {modalisOpen && <Modalcomp data ={props.data} closemodal={setModal} />}
+     
     </div>
   );
 }
--- src/pages/CardsPage/CardsPage.jsx
+++ src/pages/CardsPage/CardsPage.jsx
@@ -1,24 +1,30 @@
 import React from "react";
-import './cp.css'
+import "./cp.css";
 import ProfileCard from "../../Components/Profcard/profileCard";
+import Modalcomp from "../../Components/Modal/modal";
 import axios from "axios";
 import { useState, useEffect } from "react";
 const url = "https://api.github.com/users/atharvagadkari05/repos";
 function CardsPage() {
   const [data, setdata] = useState([]);
+  const [modalopen, setmodalopen] = useState(true);
 
   useEffect(() => {
     axios.get(url).then((response) => {
       setdata(response.data);
     });
+
+
   }, []);
 
   return (
-    <div className="Cardspage">
+    <div className="Cardspage grid grid-cols-3">
       {data.map((e) => (
-        <ProfileCard key={e.id} data={e} />
+        <ProfileCard  key={e.id} data={e} />
       ))}
 
+    {modalopen&&<Modalcomp openmodal = {setmodalopen()} />}
+     
      
     </div>
   );
